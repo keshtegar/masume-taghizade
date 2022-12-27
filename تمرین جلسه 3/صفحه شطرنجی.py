73f2b6915fdf63f6d198ccb5cc6518602f38af5f
masume taghizadeh <120125773+masumetaghizadeh@users.noreply.github.com>
2022-12-28 00:44:06 +0330

    Update صفحه شطرنجی.py

--- تمرین جلسه 3/صفحه شطرنجی.py
+++ تمرین جلسه 3/صفحه شطرنجی.py
@@ -1,9 +1,18 @@
-import numpy as np
-m=int(input("pleas enter row"))
-n=int(input("pleas enter column"))
-a=np.full((m,n),"#")
-a[1::2, ::2]="*"
-a[::2,1::2]="*"
-print(a)
+m=int(input("Please enter the number of rows"))
+n=int(input("Please enter the number of column"))
+x=1
+for i in range(1,m+1):
+    for j in range(1,n+1):
+        if x==1:
+        
+            print("#", end="")
+        else:
+            print("*", end="")
+        x=-x
+    if i*j % 2 == 0: 
+        x*=-1  
+    print()            
+
+
 
         
